dbc3e2e3c7717ac4e4c0c1815c18e671769eb511
Serenity911 <silvia.simonassi01@gmail.com>
2020-03-03 15:44:13 +0000

    setup

---
+++ src/containers/SongsContainer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SongsContainer extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            topSongs: [],
+            selectedSong: ''
+        }
+    }
+
+
+} 
